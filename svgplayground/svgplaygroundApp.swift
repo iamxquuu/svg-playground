69f7642db478ff0d7a58ffb2648a2a95a578c97f
marcel <marcel.maciaszek@icloud.com>
2023-06-02 17:49:36 +0200

    Initial Commit

---
+++ svgplayground/svgplaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  svgplaygroundApp.swift
+//  svgplayground
+//
+//  Created by Marcel Maciaszek on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct svgplaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
