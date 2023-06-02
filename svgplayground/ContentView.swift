a5277da4a9c1cfe54da7caf0f97da7df8029658e
marcel <marcel.maciaszek@icloud.com>
2023-06-02 18:01:33 +0200

    first svg upload

--- svgplayground/ContentView.swift
+++ svgplayground/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Image("maturekLOGO")
+            .resizable()
+            .scaledToFit()
+            .padding(30)
     }
 }
 
